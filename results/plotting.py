9f65bc31f04ced228d8acd01eb0b3cf1ef8affac
Naram-nm1300940 <37050634+Naram-m@users.noreply.github.com>
2024-03-30 23:33:55 +0100

    Add files via upload

--- results/plotting.py
+++ results/plotting.py
@@ -8,21 +8,21 @@ costs_optimal = np.load("./optimal.npy", allow_pickle=True)
 costs_adaftrlc = np.load("./adaftrlc.npy", allow_pickle=True)
 
 costs_gpc_cum = costs_gpc.cumsum()
-costs_optimal_cum = costs_optimal.cumsum()
+# costs_optimal_cum = costs_optimal.cumsum()
 costs_adaftrlc_cum = costs_adaftrlc.cumsum()
 
-T = len(costs_gpc)+1
+T = len(costs_gpc)
 plt.figure(figsize=(6, 5))
 
 
 ############### accum. testing ########################
-gpc_regret = (costs_gpc_cum - costs_optimal_cum)/np.arange(1, T)
-adaftrlc_regret = (costs_adaftrlc_cum - costs_optimal_cum)/np.arange(1, T)
+gpc_regret = (costs_gpc_cum - costs_optimal)/np.arange(1, T+1)
+adaftrlc_regret = (costs_adaftrlc_cum - costs_optimal)/np.arange(1, T+1)
 
 ########################################################
 
-plt.plot(np.arange(T-1)/1000, gpc_regret, color='Black', label="GPC", linewidth=2.5, markevery=2500, marker='x',  markersize=15, markeredgecolor='Grey')
-plt.plot(np.arange(T-1)/1000, adaftrlc_regret, color='Blue', label="AdaFTRL-C", linewidth=2.5, markevery=2500, marker='*',  markersize=15, markeredgecolor='Grey')
+plt.plot(np.arange(T)/1000, gpc_regret, color='Black', label="GPC", linewidth=2.5, markevery=2500, marker='x',  markersize=15, markeredgecolor='Grey')
+plt.plot(np.arange(T)/1000, adaftrlc_regret, color='Blue', label="AdaFTRL-C", linewidth=2.5, markevery=2500, marker='*',  markersize=15, markeredgecolor='Grey')
 
 
 plt.figtext(0.9, 0, r'$\times 10^3$', ha="right", fontsize=12)
@@ -35,12 +35,11 @@ plt.xlabel(r'Horizon $T$', fontsize=17)
 plt.yticks(fontsize=16, weight='bold')
 plt.xticks(fontsize=16, weight='bold')
 
-plt.savefig("./aa.pdf", bbox_inches = 'tight',pad_inches = 0)
-
+plt.savefig("./a.pdf", bbox_inches = 'tight',pad_inches = 0)
 # print ("cost gpc: ", np.sum(costs_gpc))
 # print ("cost ft: ", np.sum(costs_ftrl_c))
 # print ("cost ada: ", np.sum(costs_adaftrl_c))
-# # print ("cost optftrl-c: ", np.sum(costs_optftrl_c))
+# print ("cost optftrl-c: ", np.sum(costs_optftrl_c))
 # print ("cost optimal: ", np.sum(costs_optimal))
 
 plt.show()
